0bfcb83cce030ec5ab225ec0405e1fe143dc959e
Alexander Naumenko <alecnmk@gmail.com>
2010-10-02 16:25:22 +0300

    added ability to swap window buffers

--- init.el
+++ init.el
@@ -1,6 +1,32 @@
 ; disabling menu-bar-mode
 (menu-bar-mode -1)
 
+; swap buffers in window (by ChrisWebber)
+(setq swapping-buffer nil)
+ (setq swapping-window nil)
+ (defun swap-buffers-in-windows ()
+   "Swap buffers between two windows"
+   (interactive)
+   (if (and swapping-window
+            swapping-buffer)
+       (let ((this-buffer (current-buffer))
+             (this-window (selected-window)))
+         (if (and (window-live-p swapping-window)
+                  (buffer-live-p swapping-buffer))
+             (progn (switch-to-buffer swapping-buffer)
+                    (select-window swapping-window)
+                    (switch-to-buffer this-buffer)
+                    (select-window this-window)
+                    (message "Swapped buffers."))
+           (message "Old buffer/window killed.  Aborting."))
+         (setq swapping-buffer nil)
+         (setq swapping-window nil))
+     (progn
+       (setq swapping-buffer (current-buffer))
+       (setq swapping-window (selected-window))
+       (message "Buffer and window marked for swapping."))))
+ (global-set-key (kbd "C-c p") 'swap-buffers-in-windows)
+
 ; deleting trailing whitespace on save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
